f3e9e34ece6b1bb00853b8ce70fb32cee27d352a
Pedro Uzcategui <pedrouzcategui0103@gmail.com>
2024-03-16 12:55:32 -0400

    Progress on Memories Page

--- src/app/(dashboard)/memories/MemoryDialog.tsx
+++ src/app/(dashboard)/memories/MemoryDialog.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
@@ -8,8 +9,40 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { FAMILY_MEMBER_RELATIONSHIPS } from "@/constants/relationships";
+import { FamilyMember } from "@prisma/client";
+import { Trash } from "lucide-react";
+import React from "react";
 
-export default function MemoryDialog() {
+type MemberDialogProps = {
+    familyMembers: FamilyMember[];
+}
+
+export default function MemoryDialog({ familyMembers }: MemberDialogProps) {
+    const [preview, setPreview] = React.useState<string | ArrayBuffer | null>(null);
+    const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+    const handleImagePrevisualization = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (files && files[0]) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreview(reader.result);
+            };
+            reader.readAsDataURL(files[0]);
+        }
+    }
+
+    const handleClearImage = () => {
+        setPreview(null);
+        if (fileInputRef.current) {
+            fileInputRef.current.value = '';
+        }
+    };
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -22,9 +55,47 @@ export default function MemoryDialog() {
                         Tag your loved ones and add a description of the memory you're uploading
                     </DialogDescription>
                 </DialogHeader>
-                <DialogFooter>
-                    <Button type="submit">Create</Button>
-                </DialogFooter>
+                <form className="space-y-6">
+
+                    {preview && (<div className="flex justify-center">
+                        <img src={preview as string} className="w-[250px] h-auto" alt="File preview" />
+                    </div>)}
+
+                    <div className="grid grid-cols-3 items-center gap-2">
+                        <div className="col-span-2">
+                            <Input
+                                id="memory"
+                                name="memory"
+                                type="file"
+                                ref={fileInputRef}
+                                onChange={handleImagePrevisualization}
+                            />
+                        </div>
+                        <div>
+                            <Button type="button" onClick={handleClearImage} className="w-full"><Trash className="mr-2" /> Clear Image</Button>
+                        </div>
+                    </div>
+
+                    <div className="grid gap-2">
+                        <Label htmlFor="relationship">Members</Label>
+                        <Select
+                        >
+                            <SelectTrigger>
+                                <SelectValue placeholder="Father, Mother..." />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {familyMembers.map(({ id, name }) => (
+                                    <SelectItem key={id} value={id}>
+                                        {name}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <DialogFooter>
+                        <Button type="submit">Create</Button>
+                    </DialogFooter>
+                </form>
             </DialogContent>
         </Dialog>
     )
--- src/app/(dashboard)/memories/page.tsx
+++ src/app/(dashboard)/memories/page.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button";
 import { Memory } from "@prisma/client";
 import MemoryDialog from "./MemoryDialog";
+import { getCurrentUser } from "@/lib/user";
 
 const MOCK_MEMORIES = [
     {
@@ -41,11 +41,28 @@ const MOCK_MEMORIES = [
     },
 ] satisfies Memory[];
 
-export default function MemoriesPage() {
+export default async function MemoriesPage() {
+
+    const user = await getCurrentUser();
+    if (!user) {
+        return null;
+    }
+
+    const familyMembers = await db.familyMember.findMany({
+        where: {
+            ownerId: user.id
+        }
+    })
+
     return (
         <div>
-            <h1>Memories Page</h1>
-            <MemoryDialog />
+            <header className="mt-10">
+                <h1 className="font-bold text-4xl mb-2">Memories</h1>
+                <p>Add memories of the people you care about.</p>
+            </header>
+            <section className="my-6">
+                <MemoryDialog familyMembers={familyMembers} />
+            </section>
             <div className="grid grid-cols-4">
                 {MOCK_MEMORIES.map(memory => (
                     <div key={memory.id}>
