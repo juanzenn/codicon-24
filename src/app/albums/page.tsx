8dbf59f2f1018b7293a61fee55d2daf0df01ad39
Pedro <pedrouzcategui0103@gmail.com>
2024-03-15 23:27:20 -0400

    Layout, Menu, and Toast

---
+++ src/app/albums/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AlbumsPage() {
+    return (
+        <div>
+            <h1>Hello Page</h1>
+        </div>
+    );
+}
---
+++ src/components/Menu.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { cn } from "@/lib/utils";
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+
+const MENU_ITEMS = [
+    {
+        href: '/albums',
+        label: 'Albums'
+    },
+    {
+        href: '/memories',
+        label: 'Memories'
+    },
+    {
+        href: '/proximity',
+        label: 'Proximity'
+    },
+]
+
+export default function Menu() {
+    const pathname = usePathname();
+
+    return (
+        <nav className="w-full">
+            <ul className="flex justify-center items-center gap-4 py-2">
+                {
+                    MENU_ITEMS.map(({ href, label }, i) => (
+                        <li className={cn("p-2", { 'text-blue-800 border-b-2 border-blue-800': href === pathname })} key={`${label}-${i}`}>
+                            <Link href={href}>
+                                {label}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
+    )
+}
