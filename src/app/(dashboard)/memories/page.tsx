7d2b19fb1633e6a8c2fb4a64a7c8820b1cbbf12e
Pedro <pedrouzcategui0103@gmail.com>
2024-03-15 23:48:45 -0400

    Add Memories and Proximity Page, changes bad reference on Layout.tsx

---
+++ src/app/(dashboard)/memories/page.tsx
@@ -0,0 +1,7 @@
+export default function MemoriesPage() {
+    return (
+        <div>
+            <h1>Hello Page</h1>
+        </div>
+    );
+}
---
+++ src/app/(dashboard)/proximity/page.tsx
@@ -0,0 +1,7 @@
+export default function ProximityPage() {
+    return (
+        <div>
+            <h1>Hello Page</h1>
+        </div>
+    );
+}
