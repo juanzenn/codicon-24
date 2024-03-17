46882851531b97bcbb11bb45e530757778247bd5
Juan Alvarez <juanandres140299@gmail.com>
2024-03-17 14:45:26 -0400

    add mobile navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { LogOut } from "lucide-react";
+import { LogOut, MenuIcon } from "lucide-react";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -17,46 +17,61 @@ export default function Menu() {
   const pathname = usePathname();
 
   return (
-    <nav className="w-full bg-accent py-2 px-6 shadow-sm border-b border-b-background/30 sticky top-0 flex justify-between items-center z-50">
-      <Link
-        href="/dashboard"
-        className="font-bold text-primary text-lg tracking-tighter hover:text-primary/80 transition-colors"
-      >
-        Heritage Keeper
-      </Link>
+    <>
+      <nav className="hidden lg:flex w-full bg-accent py-2 px-6 shadow-sm border-b border-b-background/30 sticky top-0 justify-between items-center">
+        <Link
+          href="/dashboard"
+          className="font-bold text-primary text-lg tracking-tighter hover:text-primary/80 transition-colors"
+        >
+          Heritage Keeper
+        </Link>
 
-      <ul className="flex justify-center items-center gap-4 py-2">
-        {MENU_ITEMS.map(({ href, label }, i) => {
-          const isSelected = pathname === href;
+        <ul className="flex justify-center items-center gap-4 py-2">
+          {MENU_ITEMS.map(({ href, label }, i) => {
+            const isSelected = pathname === href;
 
-          return (
-            <Link
-              key={`${label}-${i}`}
-              href={href}
-              className="w-40 text-center"
-            >
-              <li
-                key={label}
-                className={cn(
-                  "font-medium px-8 py-2 rounded-lg hover:bg-primary/80 hover:text-primary-foreground transition-colors cursor-pointer",
-                  isSelected && "bg-primary text-primary-foreground",
-                )}
+            return (
+              <Link
+                key={`${label}-${i}`}
+                href={href}
+                className="w-40 text-center"
               >
-                {label}
-              </li>
-            </Link>
-          );
-        })}
-      </ul>
+                <li
+                  key={label}
+                  className={cn(
+                    "font-medium px-8 py-2 rounded-lg hover:bg-primary/80 hover:text-primary-foreground transition-colors cursor-pointer",
+                    isSelected && "bg-primary text-primary-foreground",
+                  )}
+                >
+                  {label}
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
 
-      <Button
-        className="hover:bg-destructive hover:text-destructive-foreground gap-2"
-        variant="ghost"
-        onClick={() => signOut()}
-      >
-        <LogOut size={16} />
-        Signout
-      </Button>
-    </nav>
+        <Button
+          className="hover:bg-destructive hover:text-destructive-foreground gap-2"
+          variant="ghost"
+          onClick={() => signOut()}
+        >
+          <LogOut size={16} />
+          Signout
+        </Button>
+      </nav>
+
+      <nav className="flex lg:hidden w-full bg-accent py-2 px-6 shadow-sm border-b border-b-background/30 sticky top-0 justify-between items-center">
+        <Link
+          href="/dashboard"
+          className="font-bold text-primary text-lg tracking-tighter hover:text-primary/80 transition-colors"
+        >
+          Heritage Keeper
+        </Link>
+
+        <Button>
+          <MenuIcon size={24} />
+        </Button>
+      </nav>
+    </>
   );
 }
