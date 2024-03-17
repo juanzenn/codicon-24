e9925bb8dce0749bc9ddfaa360c59e4edad905e9
Juan Alvarez <juanandres140299@gmail.com>
2024-03-17 12:40:41 -0400

    images compression on upload

--- src/hooks/useUploadImage.ts
+++ src/hooks/useUploadImage.ts
@@ -1,4 +1,5 @@
 import { useUploadThing } from "@/lib/files";
+import imageCompression from "browser-image-compression";
 import { nanoid } from "nanoid";
 
 type UploadResponse = {
@@ -9,10 +10,20 @@ export function useUploadImage() {
   const { startUpload, isUploading } = useUploadThing("imageUploader");
 
   async function uploadImage(file: File, identifier?: string) {
-    const extension = file.name.split(".").pop();
+    const compressedFile = await imageCompression(file, {
+      maxSizeMB: 4,
+      maxWidthOrHeight: 1920,
+      alwaysKeepResolution: true,
+      useWebWorker: true,
+    });
+
+    const extension = compressedFile.name.split(".").pop();
     const imageId = identifier ?? nanoid();
-    const newFile = new File([file], `${imageId}.${extension}`);
-    const [{ url }] = (await startUpload([newFile])) as UploadResponse;
+    const identifiedFile = new File(
+      [compressedFile],
+      `${imageId}.${extension}`,
+    );
+    const [{ url }] = (await startUpload([identifiedFile])) as UploadResponse;
 
     return url;
   }
